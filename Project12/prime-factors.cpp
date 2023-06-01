508e633713d64532c8976b6e8e68268dece39ee5
hyunsung.jin <darkfishno1@gmail.com>
2023-06-01 15:37:13 +0900

    Page41까지 구현

--- Project12/prime-factors.cpp
+++ Project12/prime-factors.cpp
@@ -4,10 +4,10 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int i){
+	vector<int> of(int number){
 		vector<int> result = {};
-		if( i == 2){
-			result.push_back(2);
+		if(number > 1){
+			result.push_back(number);
 		}
 		return result;
 	}
